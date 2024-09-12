ebd5ef590ff643fbad9902e52a05d70dcc000f32
Karoliine Jakovleva <karoliinejakovleva@gmail.com>
2024-09-12 09:38:30 +0300

    simple old style php website

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
-<?php
+<?php include 'partials/header.php'; ?>
 
-class Box {
-    public static $count = 0;
-    public $id;
-    public function __construct(){
-        $this->id = self::$count++;
-    }   
-    public static function setCount(){
-        //var_dump($this->id);
-        var_dump(static::$count);
-    }
- }
-class MetalBox extends Box{
+<main class="container">
 
-}
-MetalBox::name();
+<?php include 'partials/hero.php'; ?>
 
-?>
+<?php include 'partials/featured.php'; ?>
 
+
+
+  <div class="row g-5">
+    <div class="col-md-8">
+    <?php include 'partials/posts.php'; ?>
+    </div>
+
+    <div class="col-md-4">
+    <?php include 'partials/sidebar.php'; ?>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<?php include 'partials/footer.php'; ?>
