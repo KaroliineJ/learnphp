e8d57d0c29ae04ff7d5b2621e19cc985be43ee33
Karoliine Jakovleva <karoliinejakovleva@gmail.com>
2024-09-12 11:02:45 +0300

    autoloading

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-$posts = [
-    ['title' => 'Some title 1', 'body' => 'Some body 1'],
-    ['title' => 'Some title 2', 'body' => 'Some body 2'],
-    ['title' => 'Some title 3', 'body' => 'Some body 3'],
-    ['title' => 'Some title 4', 'body' => 'Some body 4'],
-];
+
 if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|js|css)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 } 
@@ -15,16 +10,61 @@ function dump(...$vars){
     echo '</pre>';
 }
 
-switch($_SERVER['REQUEST_URI']){
-    case '/':
-        include 'views/index.php';
-        break;
-    case 'us':
-        include 'views/us.php';
-        break;
-    case '/tech':
-            include 'views/tech.php';
-            break;
-    default:
-        echo 'ERROR 404';
-}
+spl_autoload_register(function($class){
+    $class = substr($class, 4); 
+    dump($class);
+    require_once "src/$class.php";
+});
+
+// require_once 'src/Router.php';
+// require_once 'src/Router.php';
+
+$router = new App\Router();
+$db = new App\DB();
+$controller = new App\Controllers\PublicController();
+dump($router, $db, $controller);
+
+//switch($_SERVER['REQUEST_URI']){
+//   case '/':
+//        $posts = [
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//            ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//             ['title' => 'Some title 2', 'body' => 'Some body 2'],
+//             ['title' => 'Some title 3', 'body' => 'Some body 3'],
+//             ['title' => 'Some title 4', 'body' => 'Some body 4'],
+//         ];
+//         include 'views/index.php';
+//         break;
+//     case 'us':
+//         $posts = [
+//             ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//             ['title' => 'Some title 2', 'body' => 'Some body 2'],
+//             ['title' => 'Some title 3', 'body' => 'Some body 3'],
+//             ['title' => 'Some title 4', 'body' => 'Some body 4'],
+//         ];
+//         include 'views/us.php';
+//         break;
+//     case '/tech':
+//         $posts = [
+//             ['title' => 'Some title 1', 'body' => 'Some body 1'],
+//             ['title' => 'Some title 2', 'body' => 'Some body 2'],
+//             ['title' => 'Some title 3', 'body' => 'Some body 3'],
+//             ['title' => 'Some title 4', 'body' => 'Some body 4'],
+//         ];
+//             include 'views/tech.php';
+//             break;
+//     default:
+//         echo 'ERROR 404';
+// }
